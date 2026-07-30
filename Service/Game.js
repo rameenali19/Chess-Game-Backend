2bdeef331dfe82e6cf614f0e50b16f740d63ff5c
rameenali19 <rameenali1910@gmail.com>
2026-07-30 23:18:30 +0500

    Getting player_color

--- Service/Game.js
+++ Service/Game.js
@@ -156,8 +156,8 @@ export class Game {
 
   //get player by games ID
   async getPlayer() {
-    const gameId = this.req.params.gameid;
     const guestId = this.req.body
+    const gameId = this.req.params.gameid;
     const player = await this.database("players")
       .where({
         game_id: gameId,

3fe5e7ac6973377fb4f42705df5c4ab51031123a
rameenali19 <rameenali1910@gmail.com>
2026-08-04 20:33:29 +0500

    sending new board and values through socket

--- Socket/socketHandler.js
+++ Socket/socketHandler.js
@@ -1,18 +1,22 @@
+
 export function socketHandler(io) {
 
   io.on("connection", (socket) => {
 
     socket.on("joinGame", ({ gameId, canJoin }) => {
-
       const resolvedRoom = parseInt(gameId);
-
       socket.join(resolvedRoom)
+
       if (canJoin) {
         io.to(resolvedRoom).emit("playerJoined")
-
       }
     })
 
-  })
+    socket.on("gameUpdate", ({ gameId, gameData }) => {
+      const resolvedRoom = parseInt(gameId);
 
+      socket.to(resolvedRoom).emit("gameUpdate", gameData)
+    })
+
+  })
 }

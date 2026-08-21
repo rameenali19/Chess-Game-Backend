15c81ac2ab129ab9dd73d90c9a3922c26959cbfb
rameenali19 <rameenali1910@gmail.com>
2026-08-21 13:40:00 +0500

    using router in gameRoutes.js

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,3 +1,7 @@
 import express from "express";
 import { gameRoutes } from "./routesConfig.js";
 import { setupRoutes } from "./routeHandler.js";
+
+const router = express.Router();
+setupRoutes(router, gameRoutes);
+export default router;

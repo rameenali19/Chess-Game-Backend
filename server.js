4390dd9d3a8b34d34325db827354c708f66d7989
rameenali19 <rameenali1910@gmail.com>
2026-08-28 00:46:52 +0500

    console statement

--- server.js
+++ server.js
@@ -49,7 +49,8 @@ socketHandler(io)
 
 //Assigning port to the server
 const PORT = process.env.PORT || 3000;
+
 httpServer.listen(PORT, () => {
-  console.log("Server is working!");
+  console.log(`Server is working on port ${PORT}!`);
 })
 
